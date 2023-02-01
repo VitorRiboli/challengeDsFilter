430e7aa8c50b9a64426dfd9cca5a3d9ad1c46c7e
vitorriboli <vitorriboli1@gmail.com>
2023-02-01 19:15:30 -0300

    Listing styles

---
+++ src/components/Listing/index.tsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+
+
+
+export default function Listing() {
+  return (
+    <main>
+      <div className="control-product-card">
+        <div className="product-card">
+          <h3>PC Gamer Pro</h3>
+          <p>R$ 1200.00</p>
+        </div>
+        <div className="product-card">
+          <h3>PC Gamer Pro</h3>
+          <p>R$ 1200.00</p>
+        </div>
+        <div className="product-card">
+          <h3>PC Gamer Pro</h3>
+          <p>R$ 1200.00</p>
+        </div>
+
+      </div>
+    </main>
+  );
+}
--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -1,9 +1,13 @@
 import Filter from "../Filter";
+import Listing from "../Listing";
+
+
 
 export default function ListingBody() {
   return (
     <main>
       <Filter />
+      <Listing />
     </main>
   )
 }
